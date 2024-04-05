ada1af42eea7b16849ddb8d15b0f84c7655a4c67
Makhmud <mahmud4563253@gmail.com>
2024-04-05 17:35:09 +0500

    added hash

---
+++ pkg/security/hash.go
@@ -0,0 +1,16 @@
+package security
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func CompareHashAndPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
